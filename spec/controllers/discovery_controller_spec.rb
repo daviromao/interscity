080495d1cb457502f4a2ecc8ef59a6ddcc621d19
Rogério Cardoso <prof.rogeriocardoso@gmail.com>
2016-05-12 18:37:06 -0300

    Atualização dos testes iniciais

--- spec/controllers/discovery_controller_spec.rb
+++ spec/controllers/discovery_controller_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe DiscoveryController, :type => :controller do
 	describe '#resources' do
-		#it 'should return status 200 OK' do
-			#expect(response.status).to eq(200)
-		#end
-		it {expect(response.status).to eq(200)}
+		it 'should return status 200 OK' do
+			get 'resources', format: :json
+			expect(response.status).to eq(200)
+		end
+		#it {expect(response.status).to eq(200)}
 	end
 
 end
