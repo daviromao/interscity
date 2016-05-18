b4e58d31bb91f20fa0752bc36396f5be986e4f01
Rogério Cardoso <prof.rogeriocardoso@gmail.com>
2016-05-17 21:35:25 -0300

    Testing mock passing params for resources controller
    Signed-off-by: @rogeriocardoso
    Signed-off-by: @juuhclemente
    Signed-off-by: @dcmspe
    Signed-off-by: @eliada

--- app/controllers/discovery_controller.rb
+++ app/controllers/discovery_controller.rb
@@ -1,5 +1,12 @@
 class DiscoveryController < ApplicationController
   def resources
-  	render status: 200
+	#render params[].each_with_index {|value, index| puts "[#{index}] == #{value}" }
+
+	render status: 400 if request.GET.size == 0
+
+	if request.GET.size > 0
+		render json: {data: request.params}, status: 200
+	end
+
   end
 end
--- spec/controllers/discovery_controller_spec.rb
+++ spec/controllers/discovery_controller_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe DiscoveryController, :type => :controller do
 	describe '#resources' do
-		it 'should return status 200 OK' do
+		it 'should return status 400 (Bad Request)' do
 			get 'resources', format: :json
-			expect(response.status).to eq(200)
+			expect(response.status).to eq(400)
 		end
 		#it {expect(response.status).to eq(200)}
+
+		it 'should return 1 params' do
+			get 'resources', params: {capability: "temp"}
+
+			expect(JSON.parse(response.body)["data"]["capability"]).
+			       to eq("temp")
+			expect(response.status).to eq(200)
+		end
+
 	end
 
 end
