e624e532da573b5e8d5047e6e7c34be10dc5f7d5
EnumaElish <henriquepotter.hp@gmail.com>
2016-06-28 15:53:31 -0300

    removing seed file content
    Signed-off-by: EnumaElish <henriquepotter.hp@gmail.com>

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-p "Dropping data"
-Resource.delete_all
-p "Creating data"
-
-res = Resource.create(name: "trafficlight", uri: "traffic_light_url", uuid: "1")
-cap = res.capabilities.create(name:"change_trafficlight")
