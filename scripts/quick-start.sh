9ec5463151a03e5c191a909a598ff4d3a4c90a88
Lucas Kanashiro <kanashiro@debian.org>
2016-09-28 15:43:31 -0300

    Remove readlink from quick_start.sh
    We forgot to do it in last commit :)
    
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>
    Signed-off-by: Lucas Kanashiro <kanashiro@debian.org>

--- scripts/quick-start.sh
+++ scripts/quick-start.sh
@@ -1,5 +1,5 @@
-FILE_DIR=`dirname $0`
-BASE_DIR=`readlink -f $FILE_DIR/../`
+FILE_DIR=`perl -e 'use Cwd "abs_path";print abs_path(shift)' $0`
+BASE_DIR=`dirname $FILE_DIR`
 PROJECT_NAME='actuator-controller'
 IMAGE_NAME=smart-cities/$PROJECT_NAME
 NETWORK='platform'
