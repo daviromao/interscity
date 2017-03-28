28335a28107fda2a0e030be9937771f7309b9ed6
Arthur Del Esposte <arthurmde@gmail.com>
2017-03-28 20:05:17 -0300

    Fixing tests

--- config/initializers/platform_setup.rb
+++ config/initializers/platform_setup.rb
@@ -1,7 +1,5 @@
 require 'yaml'
 
-if Rails.env.development? || Rails.env.production?
-  SERVICES_FILE = File.read(Rails.root.join("config", "services.yml"))
-  SERVICES_ERB = ERB.new(SERVICES_FILE)
-  SERVICES_CONFIG = YAML.load(SERVICES_ERB.result)
-end
+SERVICES_FILE = File.read(Rails.root.join("config", "services.yml"))
+SERVICES_ERB = ERB.new(SERVICES_FILE)
+SERVICES_CONFIG = YAML.load(SERVICES_ERB.result)
