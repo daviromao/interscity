d1b90cde1e57b0574136b33ff6cb71e8ffcc890c
Arthur Del Esposte <arthurmde@gmail.com>
2017-06-01 17:29:35 -0300

    Redoing actuator_controller

--- app/controllers/actuator_controller.rb
+++ app/controllers/actuator_controller.rb
@@ -1,123 +1,50 @@
-require 'rest-client'
 require 'json'
 
 class ActuatorController < ApplicationController
-
-  before_action :actuate_json_validation, :only => [:actuate]
-  before_action :validate_url_params, :only => [:cap_status]
-  after_action :actuator_capability_persistence, :only => [:actuate]
-
   def actuate
     @response = Hash.new
     @response['success'] = []
     @response['failure'] = []
+
     begin
-      actuate_params = params[:data]
-      execute_actuation(actuate_params, @response)
-      render json: @response
-    rescue Exception => e
-      render error_payload(e.message, 500)
-    end
-  end
+      command_params = params[:data]
 
-  def cap_status
-    begin
-      value_register = ActuatorValue.where(capability_id: @capability.id,
-                                           platform_resource_id: @resource.id).order('created_at DESC').first
-
-      response = {}
-      if value_register.blank?
-        response = call_to_actuator_cap_status
-      else
-        response[:data] = value_register.value
-        response[:updated_at] = value_register.created_at
-      end
-      render json: response
-    rescue RestClient::ExceptionWithResponse => e
-      render error_payload(e.response.message, e.response.code)
-    rescue Exception => e
-      render error_payload(e.message, 500)
-    end
-  end
-
-
-  private
-
-  def execute_actuation(actuate_params, response)
-    actuate_params.each { |actuator|
-      uuid = actuator['uuid']
-      capabilities = actuator['capabilities']
-      capability = capabilities.first.first
-      value = capabilities.first.second
-      begin
-        res = PlatformResource.find_by(uuid: uuid)
-        if !res.blank?
-
-          actuator_response = JSON.parse(call_to_actuator_actuate(res.uri, capability, value))
-
-          resource = actuator_response['data']
-          resource['code'] = actuator_response['code']
-          resource['capability'] = capability
-          resource['uuid'] = uuid
-          response['success'] << resource
-        else
-          response['failure'] << {uuid: actuator['uuid'], capability: capability, code: 404, message: "Resource not found"}
+      command_params.each do |actuator|
+        resource = PlatformResource.where(uuid: actuator['uuid']).first
+        unless resource
+          @response['failure'] << {
+            uuid: actuator['uuid'],
+            code: 404,
+            error: "Resource not found",
+            capability: actuator['capability'],
+            value: actuator['value']
+          }
+          next
         end
-      rescue RestClient::ExceptionWithResponse => e
-        response['failure'] << {uuid: actuator['uuid'], capability: capability, code: e.response.code, message: e.response.message}
-      rescue Exception => e
-        response['failure'] << {uuid: actuator['uuid'], capability: capability, code: 500, message: e.message}
+
+        actuator['capabilities'].each do |capability, value|
+          if resource.capabilities.include? capability
+            command = ActuatorCommand.new(
+              platform_resource: resource,
+              uuid: resource.uuid,
+              capability: capability,
+              value: value
+            )
+
+            if command.save
+              @response['success'] << command
+            else
+              @response['failure'] << {uuid: resource.uuid, capability: capability, value: value, code: 400, error: "Invalid command #{command.errors.full_messages}"}
+            end
+          else
+            @response['failure'] << {uuid: resource.uuid, capability: capability, value: value, code: 400, error: "This resource does not have such cappability"}
+          end
+        end
       end
-    }
-  end
-
-  #TODO complete resource adaptor url
-  def call_to_actuator_cap_status
-    request_url = @resource.uri + '/collect/' + @capability.name
-    JSON.parse(RestClient.get(request_url))
-  end
-
-  #TODO complete resource adaptor url
-  def call_to_actuator_actuate(actuator_url, capability, value)
-    request_url = actuator_url + '/actuate/'+ capability.to_s
-    response = RestClient.put(request_url, {data: {value: value}})
-    json_response = JSON.parse(response.body)
-    json_response[:code] = response.code
-    json_response.to_json
-  end
-
-  def actuate_json_validation
-    begin
-      params.require(:data).each { |actuator|
-        actuator.permit(:uuid, :capabilities => {}) }
-    rescue Exception => e
-      render error_payload(e.message, 400)
+      render json: @response, status: 200
+    rescue StandardError => e
+      render json: {error: e.message}, status: 400
       return false
-    end
-  end
-
-  def actuator_capability_persistence
-    begin
-      @response['success'].each do |actuator_success|
-        cap = Capability.find_by(name: actuator_success['capability'])
-        res = PlatformResource.find_by(uuid: actuator_success['uuid'])
-        ActuatorValue.create(capability_id: cap.id, platform_resource_id: res.id, value: actuator_success['state'])
-      end
-    rescue Exception => e
-    end
-  end
-
-  def validate_url_params
-    if (!params['uuid'].blank? and !params['capability'].blank?)
-      @resource = PlatformResource.find_by(uuid: params['uuid'])
-      if (@resource)
-        @capability = @resource.capabilities.find_by(name: params['capability'])
-        if (!@capability)
-          render error_payload('Capability not found', 404)
-        end
-      else
-        render error_payload('Actuator not found', 404)
-      end
     end
   end
 end
--- spec/controllers/actuator_controller_spec.rb
+++ spec/controllers/actuator_controller_spec.rb
@@ -2,232 +2,280 @@ require 'rails_helper'
 require 'json'
 
 describe ActuatorController, :type => :controller do
+  let(:json) {JSON.parse(response.body)}
+  let(:semaphore) do
+    PlatformResource.create!(uuid: '1', status: 'active', capabilities: ['semaphore'])
+  end
+  let(:lamppost) do
+    PlatformResource.create!(uuid: '2', status: 'active', capabilities: ['illuminate'])
+  end
 
-  describe '#resources' do
-
+  describe '#actuate' do
     before(:each) do
-      @controller = ActuatorController.new
-
-      allow(@controller).to receive(:call_to_actuator_cap_status).and_return({})
-
-      @res = PlatformResource.create!(uri: 'traffic_light_url', uuid: '1', collect_interval: 60, status: 'running')
-      @cap = Capability.create!(name: 'trafficlight')
-      ActuatorValue.create!(value: 'green', capability_id: @cap.id, platform_resource_id: @res.id)
-      PlatformResourceCapability.create!(capability_id: @cap.id, platform_resource_id: @res.id)
+      @semaphore = semaphore
+      @lamppost = lamppost
     end
 
-    it 'Should return status 405 code for the specific resource. Traffic light can not turn blue.' do
-      json_request = {data: [{uuid: '1', capabilities: {trafficlight: 'blue'}}]}
-      service_response = {success: [], failure: [{uuid: '1', capability: 'trafficlight', code: 405, message: "Error"}]}.to_json
-
-      fake_exception = RestClient::ExceptionWithResponse.new
-      resp = RestClient::Response
-      resp.class.module_eval { attr_accessor :code }
-      resp.class.module_eval { attr_accessor :message }
-      resp.code = 405
-      resp.message = 'Error'
-      fake_exception.response = resp
-
-      allow(@controller).to receive(:call_to_actuator_actuate).and_raise(fake_exception)
-      put :actuate, params: json_request
-      expect(response.status).to eq(200)
-      expect(response.body).to eq(service_response)
-    end
-
-    it 'Should return status 200. Traffic light actuator should be able to turn green.' do
-      json_request = {
+    context "with valid parameters" do
+      before do
+        params = {
           data: [
-              {
-                  uuid: '1',
-                  capabilities: {trafficlight: 'green'}
-              }
+            {uuid: "1", capabilities: {illuminate: "moderate"}},
+            {uuid: "1", capabilities: {semaphore: "red"}},
+            {uuid: "2", capabilities: {illuminate: "moderate"}},
+            {uuid: "2", capabilities: {illuminate: nil}},
+            {uuid: "-1", capabilities: {semaphore: "green"}},
           ]
-      }
+        }
+        process :actuate, method: :put, params: params, as: :json
+      end
+  
+      it "returns success" do
+        expect(response).to have_http_status(:success)
+      end
 
-      service_response = {
-          'success' => [
-              {
-                  'capability' => 'trafficlight',
-                  'state' => 'green',
-                  'code' => 200,
-                  'uuid' => '1'
-              }],
-          'failure' => []}
+      it "has two successful command requests" do
+        expect(json["success"].count).to eq(2)
+      end
 
-      actuator_response = {
-          data: {
-              state: 'green'
-          },
-          code: 200
-      }.to_json
+      it "has three failed command requests" do
+        expect(json["failure"].count).to eq(3)
+      end
 
+      it "returns a pending command to success requests" do
+        json["success"].each do |success|
+          expect(success).to have_key("_id")
+          expect(success).to have_key("uuid")
+          expect(success).to have_key("capability")
+          expect(success).to have_key("created_at")
+          expect(success).to have_key("updated_at")
+          expect(success).to have_key("value")
+          expect(success).to have_key("status")
+          expect(success["status"]).to eq("pending")
+        end
+      end
 
-      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
-      put :actuate, params: json_request
-      expect(response.status).to eq(200)
-      expect(JSON.parse(response.body)).to eq(service_response)
+      it "returns a detailed error description for each failure" do
+        json["failure"].each do |failure|
+          expect(failure).to have_key("uuid")
+          expect(failure).to have_key("capability")
+          expect(failure).to have_key("value")
+
+          expect(failure).to have_key("error")
+          expect(failure).to have_key("code")
+        end
+      end
+
+      it "does not returns a command object for failed requests" do
+        json["failure"].each do |failure|
+          expect(failure).to_not have_key("_id")
+          expect(failure).to_not have_key("status")
+          expect(failure).to_not have_key("created_at")
+          expect(failure).to_not have_key("updated_at")
+        end
+      end
     end
 
-    it 'create actuator value of successful actuations' do
-      json_request = {
-          data: [
-              {
-                  uuid: '1',
-                  capabilities: {trafficlight: 'green'}
-              }
-          ]
-      }
+    context "with invalid parameters" do
+      it "requires 'data' key" do
+        process :actuate,
+          method: :put,
+          params: {uuid: '1',capabilities: {semaphore: 'green'}}
 
-      actuator_response = {
-          data: {
-              state: 'green'
-          },
-          code: 200
-      }.to_json
-
-      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
-      expect { put :actuate, params: json_request }.to change { ActuatorValue.count }.by(1)
-      expect(ActuatorValue.last.value).to eq('green')
-    end
-
-    it 'does not create actuator value of successful actuations' do
-      json_request = {
-          data: [
-              {
-                  uuid: '1',
-                  capabilities: {trafficlight: 'green'}
-              }
-          ]
-      }
-
-      actuator_response = {
-          data: {
-              state: 'green'
-          },
-          code: 200
-      }.to_json
-      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
-      allow(Capability).to receive(:find_by).and_raise(Exception)
-      expect { put :actuate, params: json_request }.to_not change { ActuatorValue.count }
-    end
-
-    it 'Should return status 404. Not existing resource.' do
-      json_request = {
-          data: [
-              {
-                  uuid: '-1',
-                  capabilities: {trafficlight: 'green'}
-              }
-          ]
-      }
-
-      service_response = {
-          'success' => [],
-          'failure' => [{
-                            'capability' => 'trafficlight',
-                            'code' => 404,
-                            'uuid' => '-1',
-                            'message' => 'Resource not found'
-                        }]}
-
-      actuator_response = {
-          data: {
-              state: 'green'
-          },
-          code: 200
-      }.to_json
-
-
-      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
-      put :actuate, params: json_request
-      expect(response.status).to eq(200)
-      expect(JSON.parse(response.body)).to eq(service_response)
-    end
-
-    it 'Should return 200. Respond with error 500 when occur an internal server error while proccessing a resource actuation.' do
-      json_request = {data: [{uuid: '1', capabilities: {trafficlight: 'blue'}}]}
-      service_response = {success: [], failure: [{uuid: '1', capability: 'trafficlight', code: 500, message: "Exception"}]}.to_json
-
-      allow(@controller).to receive(:call_to_actuator_actuate).and_raise(Exception)
-      put :actuate, params: json_request
-      expect(response.status).to eq(200)
-      expect(response.body).to eq(service_response)
-    end
-
-    it 'Should return 500. Respond with error 500 when occur a general internal server error.' do
-      json_request = {data: [{uuid: '1', capabilities: {trafficlight: 'blue'}}]}
-      service_response = {code: 'InternalError', message: "Exception"}.to_json
-
-      allow(@controller).to receive(:execute_actuation).and_raise(Exception)
-      put :actuate, params: json_request
-      expect(response.status).to eq(500)
-      expect(response.body).to eq(service_response)
-    end
-
-    it 'Should return 400. Wrong json format to update a resource state.' do
-      json_request = {capability: 'trafficlight', value: 'green'}
-      put :actuate, params: json_request
-      expect(response.status).to eq(400)
-    end
-
-    it 'Should return status 200. Client requests a resource status.' do
-      ActuatorValue.create!(value: 'red', capability_id: @cap.id, platform_resource_id: @res.id)
-      url_params = {'uuid' => '1', 'capability' => 'trafficlight'}
-      actuator_response = {'data' => 'red', 'updated_at' => @res.created_at.utc.to_s}
-
-      get :cap_status, params: url_params
-
-      expect(response.status).to eq(200)
-      expect(JSON.parse(response.body)['data']).to eq (actuator_response['data'])
-      expect(Time.parse(JSON.parse(response.body)['updated_at']).utc.to_s).to eq (actuator_response['updated_at'])
-    end
-
-    it 'Should return status 404 - Not existing capability.' do
-      url_params = {uuid: '1', capability: 'temperature'}
-      service_response = {code: 'NotFound', message: 'Capability not found'}
-      get :cap_status, params: url_params
-      expect(response.status).to eq(404)
-      expect(response.body).to eq(service_response.to_json)
-    end
-
-    it 'Should return status 404 - Not existing resource.' do
-      url_params = {uuid: '2', capability: 'temperature'}
-      service_response = {code: 'NotFound', message: 'Actuator not found'}
-      get :cap_status, params: url_params
-      expect(response.status).to eq(404)
-      expect(response.body).to eq(service_response.to_json)
-    end
-
-    it 'Should return Resource adaptor status - when there is no ActuatorValue.' do
-      cap = Capability.create!(name: 'led')
-      PlatformResourceCapability.create!(capability_id: cap.id, platform_resource_id: @res.id)
-
-      url_params = {uuid: '1', capability: 'led'}
-      service_response = {'code' => 'MethodNotAllowed', 'message' => 'Error'}
-
-      fake_exception = RestClient::ExceptionWithResponse.new
-      resp = RestClient::Response
-      resp.class.module_eval { attr_accessor :code }
-      resp.class.module_eval { attr_accessor :message }
-      resp.code = 405
-      resp.message = 'Error'
-      fake_exception.response = resp
-
-      allow(@controller).to receive(:call_to_actuator_cap_status).and_raise(fake_exception)
-      get :cap_status, params: url_params
-      expect(response.status).to eq(405)
-      expect(response.body).to eq(service_response.to_json)
-    end
-    
-    it 'Should return http error 500 - when there is Internal Error.' do
-      cap = Capability.create!(name: 'led')
-      PlatformResourceCapability.create!(capability_id: cap.id, platform_resource_id: @res.id)
-
-      url_params = {uuid: '1', capability: 'led'}
-
-      allow(@controller).to receive(:call_to_actuator_cap_status).and_raise Exception
-      get :cap_status, params: url_params
-      expect(response.status).to eq(500)
+        expect(response).to have_http_status(400)
+        expect(json).to have_key("error")
+      end
     end
   end
 end
+#    it 'Should return status 200. Traffic light actuator should be able to turn green.' do
+#      json_request = {
+#      }
+#
+#      service_response = {
+#          'success' => [
+#              {
+#                  'capability' => 'trafficlight',
+#                  'state' => 'green',
+#                  'code' => 200,
+#                  'uuid' => '1'
+#              }],
+#          'failure' => []}
+#
+#      actuator_response = {
+#          data: {
+#              state: 'green'
+#          },
+#          code: 200
+#      }.to_json
+#
+#
+#      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
+#      put :actuate, params: json_request
+#      expect(response.status).to eq(200)
+#      expect(JSON.parse(response.body)).to eq(service_response)
+#    end
+#
+#    it 'create actuator value of successful actuations' do
+#      json_request = {
+#          data: [
+#              {
+#                  uuid: '1',
+#                  capabilities: {trafficlight: 'green'}
+#              }
+#          ]
+#      }
+#
+#      actuator_response = {
+#          data: {
+#              state: 'green'
+#          },
+#          code: 200
+#      }.to_json
+#
+#      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
+#      expect { put :actuate, params: json_request }.to change { ActuatorValue.count }.by(1)
+#      expect(ActuatorValue.last.value).to eq('green')
+#    end
+#
+#    it 'does not create actuator value of successful actuations' do
+#      json_request = {
+#          data: [
+#              {
+#                  uuid: '1',
+#                  capabilities: {trafficlight: 'green'}
+#              }
+#          ]
+#      }
+#
+#      actuator_response = {
+#          data: {
+#              state: 'green'
+#          },
+#          code: 200
+#      }.to_json
+#      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
+#      allow(Capability).to receive(:find_by).and_raise(Exception)
+#      expect { put :actuate, params: json_request }.to_not change { ActuatorValue.count }
+#    end
+#
+#    it 'Should return status 404. Not existing resource.' do
+#      json_request = {
+#          data: [
+#              {
+#                  uuid: '-1',
+#                  capabilities: {trafficlight: 'green'}
+#              }
+#          ]
+#      }
+#
+#      service_response = {
+#          'success' => [],
+#          'failure' => [{
+#                            'capability' => 'trafficlight',
+#                            'code' => 404,
+#                            'uuid' => '-1',
+#                            'message' => 'Resource not found'
+#                        }]}
+#
+#      actuator_response = {
+#          data: {
+#              state: 'green'
+#          },
+#          code: 200
+#      }.to_json
+#
+#
+#      allow(@controller).to receive(:call_to_actuator_actuate).and_return(actuator_response)
+#      put :actuate, params: json_request
+#      expect(response.status).to eq(200)
+#      expect(JSON.parse(response.body)).to eq(service_response)
+#    end
+#
+#    it 'Should return 200. Respond with error 500 when occur an internal server error while proccessing a resource actuation.' do
+#      json_request = {data: [{uuid: '1', capabilities: {trafficlight: 'blue'}}]}
+#      service_response = {success: [], failure: [{uuid: '1', capability: 'trafficlight', code: 500, message: "Exception"}]}.to_json
+#
+#      allow(@controller).to receive(:call_to_actuator_actuate).and_raise(Exception)
+#      put :actuate, params: json_request
+#      expect(response.status).to eq(200)
+#      expect(response.body).to eq(service_response)
+#    end
+#
+#    it 'Should return 500. Respond with error 500 when occur a general internal server error.' do
+#      json_request = {data: [{uuid: '1', capabilities: {trafficlight: 'blue'}}]}
+#      service_response = {code: 'InternalError', message: "Exception"}.to_json
+#
+#      allow(@controller).to receive(:execute_actuation).and_raise(Exception)
+#      put :actuate, params: json_request
+#      expect(response.status).to eq(500)
+#      expect(response.body).to eq(service_response)
+#    end
+#
+#    it 'Should return 400. Wrong json format to update a resource state.' do
+#      json_request = {capability: 'trafficlight', value: 'green'}
+#      put :actuate, params: json_request
+#      expect(response.status).to eq(400)
+#    end
+#
+#    it 'Should return status 200. Client requests a resource status.' do
+#      ActuatorValue.create!(value: 'red', capability_id: @cap.id, platform_resource_id: @res.id)
+#      url_params = {'uuid' => '1', 'capability' => 'trafficlight'}
+#      actuator_response = {'data' => 'red', 'updated_at' => @res.created_at.utc.to_s}
+#
+#      get :cap_status, params: url_params
+#
+#      expect(response.status).to eq(200)
+#      expect(JSON.parse(response.body)['data']).to eq (actuator_response['data'])
+#      expect(Time.parse(JSON.parse(response.body)['updated_at']).utc.to_s).to eq (actuator_response['updated_at'])
+#    end
+#
+#    it 'Should return status 404 - Not existing capability.' do
+#      url_params = {uuid: '1', capability: 'temperature'}
+#      service_response = {code: 'NotFound', message: 'Capability not found'}
+#      get :cap_status, params: url_params
+#      expect(response.status).to eq(404)
+#      expect(response.body).to eq(service_response.to_json)
+#    end
+#
+#    it 'Should return status 404 - Not existing resource.' do
+#      url_params = {uuid: '2', capability: 'temperature'}
+#      service_response = {code: 'NotFound', message: 'Actuator not found'}
+#      get :cap_status, params: url_params
+#      expect(response.status).to eq(404)
+#      expect(response.body).to eq(service_response.to_json)
+#    end
+#
+#    it 'Should return Resource adaptor status - when there is no ActuatorValue.' do
+#      cap = Capability.create!(name: 'led')
+#      PlatformResourceCapability.create!(capability_id: cap.id, platform_resource_id: @res.id)
+#
+#      url_params = {uuid: '1', capability: 'led'}
+#      service_response = {'code' => 'MethodNotAllowed', 'message' => 'Error'}
+#
+#      fake_exception = RestClient::ExceptionWithResponse.new
+#      resp = RestClient::Response
+#      resp.class.module_eval { attr_accessor :code }
+#      resp.class.module_eval { attr_accessor :message }
+#      resp.code = 405
+#      resp.message = 'Error'
+#      fake_exception.response = resp
+#
+#      allow(@controller).to receive(:call_to_actuator_cap_status).and_raise(fake_exception)
+#      get :cap_status, params: url_params
+#      expect(response.status).to eq(405)
+#      expect(response.body).to eq(service_response.to_json)
+#    end
+#    
+#    it 'Should return http error 500 - when there is Internal Error.' do
+#      cap = Capability.create!(name: 'led')
+#      PlatformResourceCapability.create!(capability_id: cap.id, platform_resource_id: @res.id)
+#
+#      url_params = {uuid: '1', capability: 'led'}
+#
+#      allow(@controller).to receive(:call_to_actuator_cap_status).and_raise Exception
+#      get :cap_status, params: url_params
+#      expect(response.status).to eq(500)
+#    end
+#  end
+#end
