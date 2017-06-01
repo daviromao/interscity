a943f66e45ba20c61d044c70bcf3b073c5cbfc60
Arthur Del Esposte <arthurmde@gmail.com>
2017-06-01 10:23:24 -0300

    Remove worker initialize for test env

--- config/initializers/workers.rb
+++ config/initializers/workers.rb
@@ -1,14 +1,15 @@
-WORKERS_LOGGER ||= Logger.new("#{Rails.root}/log/workers.log")
+if Rails.env.development? || Rails.env.production?
+  WORKERS_LOGGER ||= Logger.new("#{Rails.root}/log/workers.log")
 
-$conn = Bunny.new(
-  hostname: SERVICES_CONFIG['services']['rabbitmq'],
-  logger: WORKERS_LOGGER,
-)
-$conn.start
+  $conn = Bunny.new(
+    hostname: SERVICES_CONFIG['services']['rabbitmq'],
+    logger: WORKERS_LOGGER,
+  )
+  $conn.start
 
-resource_creator_worker = ResourceCreator.new(2, 2)
-resource_creator_worker.perform
+  resource_creator_worker = ResourceCreator.new(2, 2)
+  resource_creator_worker.perform
 
-resource_updater_worker = ResourceUpdater.new(1, 1)
-resource_updater_worker.perform
-
+  resource_updater_worker = ResourceUpdater.new(1, 1)
+  resource_updater_worker.perform
+end
