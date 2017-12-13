fd7e7eb4d0549f7714cc640de4caf62c933f1886
Arthur Del Esposte <arthurmde@gmail.com>
2017-12-13 14:14:00 -0200

    Add redis to support memory-based queries

--- Gemfile
+++ Gemfile
@@ -13,7 +13,8 @@ gem 'kong'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.0'
 # Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 3.0'
+gem 'redis', '~> 3.0'
+gem 'mock_redis'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/initializers/redis.rb
@@ -0,0 +1,11 @@
+require 'mock_redis'
+
+if Rails.env.development? || Rails.env.production?
+  host = ENV["REDIS_HOST"] || "redis"
+  port = ENV["REDIS_PORT"] || 6380
+  db = ENV["REDIS_DB"] || "capabilities"
+  $redis = Redis.new(host: host, port: port, db: db)
+else # Test environment
+  $redis = MockRedis.new
+end
+
