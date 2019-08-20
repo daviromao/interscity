0d90e6c348ee2243671376ee290fcd34c0bf519b
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-08-19 21:24:24 -0300

    Remove RSpec from service-base gem
    It was added automatically as part of the gem creation. But the gem has
    no code so there is nothing to test. So for the sake of simplicity it is
    removed now and if necessary in the future this commit can be reverted.

--- src/gems/service-base/spec/service/base_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Service::Base do
-  it "has a version number" do
-    expect(Service::Base::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
