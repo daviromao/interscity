8d88d4428fed6da7c914a3ca17fb8f877d35f3b2
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-08-22 17:12:00 -0300

    Stop using global variable for actuator-controller worker connection
    This is just a bad practice and there are good alternatives such as
    https://guides.rubyonrails.org/configuring.html#custom-configuration.
    
    Rubocop error:
    
    ```
    actuator-controller/app/workers/actuator_command_updater.rb:16:16: C: Style/GlobalVars: Do not introduce global variables.
        @channel = $conn.create_channel(nil, thread_pool)
                   ^^^^^
    actuator-controller/app/workers/resource_creator.rb:16:16: C: Style/GlobalVars: Do not introduce global variables.
        @channel = $conn.create_channel(nil, thread_pool)
    ```

--- src/actuator-controller/config/initializers/workers.rb
+++ src/actuator-controller/config/initializers/workers.rb
@@ -3,11 +3,11 @@ if Rails.env.development? || Rails.env.production?
 
   WORKERS_LOGGER ||= Logger.new("#{Rails.root}/log/workers.log")
 
-  $conn = Bunny.new(
+  Rails.configuration.worker.conn = Bunny.new(
     hostname: SERVICES_CONFIG['services']['rabbitmq'],
     logger: WORKERS_LOGGER,
   )
-  $conn.start
+  Rails.configuration.worker.conn.start
 
   resource_creator_worker = ResourceCreator.new(2, 2)
   resource_creator_worker.perform
