d7e69484c8b97b937bd5bca48647526b7fcd15c3
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-09-02 17:45:23 -0300

    Apply rubocop auto fixes after rebase

--- src/gems/service-base/lib/service/base/kong_lib/wrapper.rb
+++ src/gems/service-base/lib/service/base/kong_lib/wrapper.rb
@@ -1,4 +1,6 @@
-require "kong"
+# frozen_string_literal: true
+
+require 'kong'
 
 module Service
   module Base
@@ -11,53 +13,48 @@ module Service
 
         def register_as_target(name, upstream_name, weight = 100)
           if name.blank? || upstream_name.blank?
-            raise "Missing required parameters for proxy configuration: name, upstream_name"
+            raise 'Missing required parameters for proxy configuration: name, upstream_name'
           end
 
-          upstream = Kong::Upstream.find_by_name(upstream_name)
+          upstream = Kong::Upstream.find_by(name: upstream_name)
           if upstream.nil?
-            upstream = Kong::Upstream.new({ name: upstream_name })
+            upstream = Kong::Upstream.new(name: upstream_name)
             upstream.save
           end
 
-          api = Kong::Api.find_by_name(name)
+          api = Kong::Api.find_by(name: name)
           if api.nil?
             api = Kong::Api.new(
-              {
-                name: name,
-                upstream_url: "http://#{upstream_name}",
-                uris: "/#{name}",
-                strip_uri: true
-              }
+              name: name,
+              upstream_url: "http://#{upstream_name}",
+              uris: "/#{name}",
+              strip_uri: true
             ).save
           end
 
           Kong::Target.new(
-            {
-              upstream_id: upstream.id,
-              target: @self_host,
-              weight: weight,
-            }
+            upstream_id: upstream.id,
+            target: @self_host,
+            weight: weight
           ).save
 
-          Rails.logger.info "Target was succesfully registered to Kong"
+          Rails.logger.info 'Target was succesfully registered to Kong'
         rescue StandardError => e
           raise StandardError, "Could not register target to Kong #{e.message}"
         end
 
         def register_as_api(name, uris)
-          if name.blank? || uris.blank?
-            raise "Missing required parameters for proxy configuration: name, uris"
-          end
-          api = Kong::Api.find_by_name(name)
-          api = Kong::Api.new({
+          raise 'Missing required parameters for proxy configuration: name, uris' if name.blank? || uris.blank?
+
+          api = Kong::Api.find_by(name: name)
+          api = Kong::Api.new(
             name: name,
             upstream_url: @self_host_with_http,
             uris: uris,
             strip_uri: true
-          })
+          )
 
-          Rails.logger.info "API was succesfully registered to Kong"
+          Rails.logger.info 'API was succesfully registered to Kong'
         rescue StandardError => e
           Rails.logger.error "Could not register API to Kong #{e.message}"
         end
