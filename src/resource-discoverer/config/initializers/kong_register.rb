82bfaca0d379a3722610e9af596f6dbeef7728fc
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-09-02 17:25:30 -0300

    Use Kong wrapper from service-base for resource-discoverer service
    This makes easier the maintenance of this service.

--- src/resource-discoverer/config/initializers/kong_register.rb
+++ src/resource-discoverer/config/initializers/kong_register.rb
@@ -1,10 +1,11 @@
 require "rest-client"
+require 'service/base/kong_lib/wrapper'
 
 if Rails.env.development? || Rails.env.production?
   register_method = ENV['REGISTER_METHOD'] || 'target'
   self_host = SERVICES_CONFIG['services']['self'] || 'resource-discoverer:3000'
 
-  wrapper = Kong::Wrapper.new(self_host)
+  wrapper = Service::Base::KongLib::Wrapper.new(self_host)
 
   if register_method == 'target'
     wrapper.register_as_target('discovery', 'discovery.v1.service', weight = 100)
