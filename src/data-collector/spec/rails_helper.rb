5ea2a7fb54ce33e562bd05fb78181885eedbe8b0
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-09-05 11:55:38 -0300

    Improve spec support files require loop
    Rubocop raised the following error:
    
    ```
    data-collector/spec/rails_helper.rb:11:5: C: Rails/FilePath: Please use Rails.root.join('path', 'to') instead.
    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    ```

--- src/data-collector/spec/rails_helper.rb
+++ src/data-collector/spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'rspec/rails'
 require 'rspec/expectations'
 # Add additional requires below this line. Rails is not loaded until this
 # point!
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
