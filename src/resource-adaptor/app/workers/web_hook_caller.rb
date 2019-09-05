49f92b22c9cb871c5bf1e2493126439e4deeb4f4
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-09-05 14:28:48 -0300

    Improve resource-adaptor WebHookCaller code
    This addresses rubocop issues:
    
    ```
    resource-adaptor/app/workers/web_hook_caller.rb:11:3: C: Metrics/AbcSize: Assignment Branch Condition size for perform is too high. [20.9/15]
      def perform(id, url, body) ...
      ^^^^^^^^^^^^^^^^^^^^^^^^^^
    resource-adaptor/app/workers/web_hook_caller.rb:11:3: C: Metrics/MethodLength: Method has too many lines. [24/20]
      def perform(id, url, body) ...
      ^^^^^^^^^^^^^^^^^^^^^^^^^^
    resource-adaptor/app/workers/web_hook_caller.rb:23:7: W: Lint/UselessAssignment: Useless assignment to variable - response.
          response = RestClient.post(
          ^^^^^^^^
    resource-adaptor/app/workers/web_hook_caller.rb:30:121: C: Metrics/LineLength: Line is too long. [124/120]
          DataManager.instance.publish_actuation_command_status(command['uuid'], command['capability'], command_id, 'processed')
                                                                                                                            ^^^^
    resource-adaptor/app/workers/web_hook_caller.rb:33:121: C: Metrics/LineLength: Line is too long. [123/120]
          DataManager.instance.publish_actuation_command_status(command['uuid'], command['capability'], command_id, 'rejected')
                                                                                                                            ^^^
    ```

--- src/resource-adaptor/app/workers/web_hook_caller.rb
+++ src/resource-adaptor/app/workers/web_hook_caller.rb
@@ -20,20 +20,33 @@ class WebHookCaller
     command_id = command['_id']['$oid']
 
     begin
-      response = RestClient.post(
-        url,
-        { action: 'actuator_command', command: command }.to_json,
-        content_type: :json, accept: :json
-      )
-
-      WORKERS_LOGGER.info("WebHookCaller::CommandSend - notification_id: #{id}, url: #{url}")
-      DataManager.instance.publish_actuation_command_status(command['uuid'], command['capability'], command_id, 'processed')
+      call_webhook(command, id, url)
     rescue RestClient::ExceptionWithResponse => e
       WORKERS_LOGGER.error("WebHookCaller::CommandNotSend - notification_id: #{id}, url: #{url}, error: #{e.message}")
-      DataManager.instance.publish_actuation_command_status(command['uuid'], command['capability'], command_id, 'rejected')
+      DataManager
+        .instance
+        .publish_actuation_command_status(command['uuid'], command['capability'], command_id, 'rejected')
     rescue StandardError => e
       WORKERS_LOGGER.error("WebHookCaller::CommandNotSend - notification_id: #{id}, url: #{url}, error: #{e.message}")
       raise e # This will make sidekiq to retry again later
     end
+  end
+
+  private
+
+  def call_webhook(command, id, url)
+    RestClient.post(
+      url,
+      { action: 'actuator_command', command: command }.to_json,
+      content_type: :json, accept: :json
+    )
+
+    WORKERS_LOGGER.info("WebHookCaller::CommandSend - notification_id: #{id}, url: #{url}")
+    DataManager.instance.publish_actuation_command_status(
+      command['uuid'],
+      command['capability'],
+      command_id,
+      'processed'
+    )
   end
 end
