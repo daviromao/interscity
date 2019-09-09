5e6cda671feebc27adc0295e3c2710b7ddb38f13
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-09-09 10:54:03 -0300

    Improve resource-cataloguer Location code
    Rubocop message:
    
    ```
    resource-cataloguer/lib/location.rb:9:7: C: Style/GuardClause: Use a guard clause (return nil if first_pc.nil?) instead of wrapping the code inside a conditional expression.
          if first_pc.nil?
          ^^
    ```

--- src/resource-cataloguer/lib/location.rb
+++ src/resource-cataloguer/lib/location.rb
@@ -6,18 +6,18 @@ module SmartCities
       return nil if results.blank?
 
       first_pc = results.first.postal_code
-      if first_pc.nil?
-        return nil
-      elsif first_pc.length == 9 # Length of a postal code in BR
-        return first_pc
-      else # The first postal code has only 5 digits
-        results.drop(1).each do |result|
-          result_pc = result.postal_code
-          return result_pc if /#{first_pc}-[0-9]{3}/.match result_pc
-        end
-        # There is no postal code with 9 digits
-        return first_pc << '000'
+      return nil if first_pc.nil?
+
+      return first_pc if first_pc.length == 9 # Length of a postal code in BR
+
+      # The first postal code has only 5 digits
+      results.drop(1).each do |result|
+        result_pc = result.postal_code
+        return result_pc if /#{first_pc}-[0-9]{3}/.match result_pc
       end
+
+      # There is no postal code with 9 digits
+      first_pc << '000'
     end
 
     def self.get_neighborhood(address)
