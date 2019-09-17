daviromao/interscity

commit be6a5743f0f8930e2bc76a522b25e3f932c297ee

Apply rubocop auto-fixes after rebase

src/gems/service-base/lib/service/base/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Service
   module Base
-    VERSION = "0.19.0"
+    VERSION = '0.19.0'
   end
 end

src/gems/service-base/service-base.gemspec
@@ -1,38 +1,38 @@
+# frozen_string_literal: true
 
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "service/base/version"
+require 'service/base/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "service-base"
+  spec.name          = 'service-base'
   spec.version       = Service::Base::VERSION
-  spec.authors       = ["Rafael Reggiani Manzo"]
-  spec.email         = ["rr.manzo@protonmail.com"]
+  spec.authors       = ['Rafael Reggiani Manzo']
+  spec.email         = ['rr.manzo@protonmail.com']
 
-  spec.summary       = %q{Base for InterSCity services.}
-  spec.description   = %q{It is expected to hold gems and configuration files that are shared by more than one service. Its main objective is to eliminate repetitions between services making maintenance easier.}
-  spec.homepage      = "https://gitlab.com/interscity/interscity-platform/interscity-platform"
+  spec.summary       = 'Base for InterSCity services.'
+  spec.description   = 'It is expected to hold gems and configuration files that are shared by more than one service. Its main objective is to eliminate repetitions between services making maintenance easier.'
+  spec.homepage      = 'https://gitlab.com/interscity/interscity-platform/interscity-platform'
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
+  spec.files         = Dir.chdir(File.expand_path(__dir__)) do
     `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_dependency 'rails', '~> 5.2.3'
-  spec.add_dependency 'sqlite3', '~> 1.4'
-  spec.add_dependency 'pg', '~> 1.1'
-  spec.add_dependency 'kong', '~> 0.3.1'
-  spec.add_dependency 'puma', '~> 3.11'
-  spec.add_dependency 'rack-cors', '~> 1.0.3'
-  spec.add_dependency 'rest-client', '~> 2.0.2'
   spec.add_dependency 'bunny', '~> 2.14.2'
   spec.add_dependency 'jbuilder', '~> 2.0'
+  spec.add_dependency 'kong', '~> 0.3.1'
+  spec.add_dependency 'pg', '~> 1.1'
+  spec.add_dependency 'rack-cors', '~> 1.0.3'
+  spec.add_dependency 'rails', '~> 5.2.3'
+  spec.add_dependency 'rest-client', '~> 2.0.2'
+  spec.add_dependency 'sqlite3', '~> 1.4'
   spec.add_dependency 'bootsnap', '>= 1.1.0'
 
-  spec.add_development_dependency "bundler", "~> 2.0.2"
-  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency 'bundler', '~> 1.17'
+  spec.add_development_dependency 'rake', '~> 10.0'
 end

src/actuator-controller/Gemfile
@@ -6,11 +6,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.4'
 
 gem 'database_cleaner'
+gem 'faye'
+gem 'jquery-rails'
 gem 'kaminari-mongoid'
 gem 'mongoid'
 gem 'sass-rails', '~> 5.0'
-gem 'jquery-rails'
-gem 'faye'
 gem 'thin'
 
 # Configure application

src/data-collector/Gemfile
@@ -5,12 +5,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.4'
 
-gem 'rake'
 gem 'database_cleaner'
-gem 'mongoid'
-gem 'sass-rails', '~> 5.0'
-gem 'jquery-rails'
 gem 'faye'
+gem 'jquery-rails'
+gem 'mongoid'
+gem 'rake'
+gem 'sass-rails', '~> 5.0'
 gem 'thin'
 
 # Configure application

src/data-collector/spec/controllers/sensor_values_range_filter_spec.rb
@@ -599,9 +599,9 @@ RSpec.describe SensorValuesController, type: :controller do
       it 'returns the last value for multiple uuids' do
         %i[post get].each do |verb|
           send(verb, 'resources_data_last', params: {
-                 uuids: [
-                   '2de545ae-841a-4e4a-b961-a43bb324a2b9',
-                   '989e93f2-35e1-4a2b-b80a-4bf91030085c'
+                 uuids: %w[
+                   2de545ae-841a-4e4a-b961-a43bb324a2b9
+                   989e93f2-35e1-4a2b-b80a-4bf91030085c
                  ]
                })
           returned_json = JSON.parse(response.body)

src/gems/service-base/Gemfile
@@ -1,6 +1,8 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+source 'https://rubygems.org'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in service-base.gemspec
 gemspec

src/gems/service-base/Rakefile
@@ -1,6 +1,8 @@
-require "bundler/gem_tasks"
-require "rspec/core/rake_task"
+# frozen_string_literal: true
+
+require 'bundler/gem_tasks'
+require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new(:spec)
 
-task :default => :spec
+task default: :spec

src/gems/service-base/lib/service/base.rb
@@ -1,4 +1,6 @@
-require "service/base/version"
+# frozen_string_literal: true
+
+require 'service/base/version'
 
 module Service
   module Base

commit 9ceb9b31c2f0cfa5b120139063672254209c4306

Fix missing puma dependency
It seems that the services use Thin as the server when running `rails
s`. This led us to not notice that puma was missing until trying to
deploy to production. Under production, puma is directly invoked to
bring up the server.

This was a mistake introduced by commit
be6a5743f0f8930e2bc76a522b25e3f932c297ee.

src/gems/service-base/lib/service/base/version.rb
@@ -2,6 +2,6 @@
 
 module Service
   module Base
-    VERSION = '0.19.1'
+    VERSION = '0.19.2'
   end
 end

src/gems/service-base/service-base.gemspec
@@ -28,6 +28,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'jbuilder', '~> 2.0'
   spec.add_dependency 'kong', '~> 0.3.1'
   spec.add_dependency 'pg', '~> 1.1'
+  spec.add_dependency 'puma', '~> 3.11'
   spec.add_dependency 'rack-cors', '~> 1.0.3'
   spec.add_dependency 'rails', '~> 5.2.3'
   spec.add_dependency 'rest-client', '~> 2.0.2'