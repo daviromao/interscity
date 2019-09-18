8948f3b931e4881094c3a2bae38ed61ca9dd40c6
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-09-18 13:47:45 -0300

    Create integration test catalog capability creation

--- src/test/spec/catalog_spec.rb
+++ src/test/spec/catalog_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'catalog' do
     end
 
     it 'is expected to respond with success' do
-      expect(@response.status).to be(200)
+      expect(@response.status).to be(201) # 201 - Created
     end
 
     context 'JSON format' do
@@ -38,6 +38,31 @@ RSpec.describe 'catalog' do
       it 'is expected to have an Array as value to the "capabilities" key' do
         expect(@json['capabilities']).to be_an(Array)
       end
+    end
+  end
+
+  describe 'POST capabilities' do
+    let(:name) { 'temperature' }
+    let(:description) { 'Environment temperature' }
+    let(:type) { 'sensor' }
+
+    before do
+      @response = connection.post(
+        'catalog/capabilities',
+        {
+          name: name,
+          description: description,
+          capability_type: type
+        }
+      )
+    end
+
+    it 'is expected to respond with success' do
+      expect(@response.status).to be(201) # 201 - Created
+    end
+
+    after do
+      connection.delete("catalog/capabilities/#{name}")
     end
   end
 end
