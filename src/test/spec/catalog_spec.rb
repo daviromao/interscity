d221391d137c2e78c6573b4758f3db7bad1361ea
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2019-09-24 08:45:59 -0300

    Extract duplicated integration test code to shared example

--- src/test/spec/catalog_spec.rb
+++ src/test/spec/catalog_spec.rb
@@ -7,6 +7,30 @@ RSpec.configure do |c|
   c.include RequestsHelper
 end
 
+RSpec.shared_examples 'capability search' do |path, hash_key|
+  before do
+    @response = connection.get("catalog/#{path}")
+  end
+
+  it 'is expected to respond with success' do
+    expect(@response.status).to be(200)
+  end
+
+  context 'JSON format' do
+    before do
+      @json = response_json(@response)
+    end
+
+    it 'is expected to have the "capabilities" key' do
+      expect(@json.key?(hash_key)).to be true
+    end
+
+    it "is expected to have an Array as value to the \"#{hash_key}\" key" do
+      expect(@json[hash_key]).to be_an(Array)
+    end
+  end
+end
+
 RSpec.describe 'catalog' do
   describe 'GET resources' do
     it 'is expected to respond with success' do
@@ -17,28 +41,7 @@ RSpec.describe 'catalog' do
   end
 
   describe 'GET capabilities' do
-    before do
-      @response = connection.get('catalog/capabilities')
-      @json = response_json(@response)
-    end
-
-    it 'is expected to respond with success' do
-      expect(@response.status).to be(201) # 201 - Created
-    end
-
-    context 'JSON format' do
-      before do
-        @json = response_json(@response)
-      end
-
-      it 'is expected to have the "capabilities" key' do
-        expect(@json.key?('capabilities')).to be true
-      end
-
-      it 'is expected to have an Array as value to the "capabilities" key' do
-        expect(@json['capabilities']).to be_an(Array)
-      end
-    end
+    include_examples 'capability search', 'capabilities', 'capabilities'
   end
 
   describe 'POST capabilities' do
@@ -89,27 +92,6 @@ RSpec.describe 'catalog' do
   end
 
   describe 'GET resources/sensors' do
-    before do
-      @response = connection.get('catalog/resources/sensors')
-      @json = response_json(@response)
-    end
-
-    it 'is expected to respond with success' do
-      expect(@response.status).to be(200)
-    end
-
-    context 'JSON format' do
-      before do
-        @json = response_json(@response)
-      end
-
-      it 'is expected to have the "capabilities" key' do
-        expect(@json.key?('resources')).to be true
-      end
-
-      it 'is expected to have an Array as value to the "capabilities" key' do
-        expect(@json['resources']).to be_an(Array)
-      end
-    end
+    include_examples 'capability search', 'resources/sensors', 'resources'
   end
 end
