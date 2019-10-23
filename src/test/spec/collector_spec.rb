30b0d1618efb954c174d3161487ef440ca4034a5
Eduardo Silva <esilva.pragmacode@protonmail.ch>
2019-10-23 18:25:12 -0300

    Create integration tests for data collector

--- src/test/spec/collector_spec.rb
+++ src/test/spec/collector_spec.rb
@@ -7,12 +7,144 @@ RSpec.configure do |c|
   c.include RequestsHelper
 end
 
-RSpec.describe 'collector' do
-  describe 'GET resources data' do
-    it 'is expected to respond with success' do
-      response = connection.get('collector/resources/data')
+RSpec.describe '/collector' do
+  describe '/resources' do
+    name = 'temperature'
+    description = 'Environment temperature'
+    type = 'sensor'
+    status = 'active'
+    lat = -23.559616
+    lon = -46.731386
+    data = [
+      { temperature: 10, timestamp: '2017-06-14T17:52:25.428Z' },
+      { temperature: 10, timestamp: '2017-06-15T17:52:25.428Z' }
+    ]
 
-      expect(response.status).to be(200)
+    before :all do
+      connection.post(
+        'catalog/capabilities',
+        name: name,
+        description: description,
+        capability_type: type
+      )
+
+      response = connection.post(
+        'adaptor/resources',
+        data: {
+          description: description,
+          capabilities: [name],
+          status: status,
+          lat: lat,
+          lon: lon
+        }
+      )
+
+      @uuid = response_json(response)['data']['uuid']
+
+      sleep 0.5
+
+      connection.post(
+        "adaptor/resources/#{@uuid}/data",
+        data: {
+          environment_monitoring: data
+        }
+      )
+
+      connection.headers = { 'Content-Type' => 'application/json' }
+
+      sleep 0.5
+    end
+
+    after :all do
+      connection.delete("catalog/capabilities/#{name}")
+    end
+
+    describe '/data' do
+      describe 'POST /' do
+        before do
+          connection.headers = { 'Content-Type' => 'application/json' }
+          @response = connection.post('collector/resources/data')
+        end
+
+        it 'is expected to respond with success' do
+          expect(@response.status).to be(200)
+        end
+
+        it 'is expected to return a list of resources' do
+          json = response_json(@response)
+
+          expect(json['resources']).to be_an_instance_of(Array)
+        end
+
+        it 'is expected to have some capability data' do
+          resource_data = response_json(@response)['resources'].first
+          capability_data = resource_data.dig(
+            'capabilities',
+            'environment_monitoring'
+          ).first
+
+          expect(capability_data[name]).to eq(data.first[name.to_sym])
+        end
+      end
+
+      describe '/last' do
+        describe 'POST /' do
+          before do
+            connection.headers = { 'Content-Type' => 'application/json' }
+            @response = connection.post('collector/resources/data/last')
+          end
+
+          it 'is expected to respond with success' do
+            expect(@response.status).to be(200)
+          end
+
+          it 'is expected to return a list of resources' do
+            json = response_json(@response)
+
+            expect(json['resources']).to be_an_instance_of(Array)
+          end
+        end
+      end
+    end
+
+    describe '/{uuid}' do
+      describe '/data' do
+        describe 'POST /' do
+          before do
+            @response = connection.post("collector/resources/#{@uuid}/data")
+          end
+
+          it 'is expected to respond with success' do
+            expect(@response.status).to be(200)
+          end
+
+          it 'is expected to return a list of resources' do
+            json = response_json(@response)
+
+            expect(json['resources']).to be_an_instance_of(Array)
+          end
+        end
+
+        describe '/last' do
+          describe 'POST /' do
+            before do
+              @response = connection.post(
+                "collector/resources/#{@uuid}/data/last"
+              )
+            end
+
+            it 'is expected to respond with success' do
+              expect(@response.status).to be(200)
+            end
+
+            it 'is expected to return a list of resources' do
+              json = response_json(@response)
+
+              expect(json['resources']).to be_an_instance_of(Array)
+            end
+          end
+        end
+      end
     end
   end
 end
