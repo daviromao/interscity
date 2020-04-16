98f3300280a17ae83458307ee298d988c26339be
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2020-04-16 17:06:35 -0300

    Create integration test to replicate user bug
    This was reported by a user and I could not replicate it with our
    current test suite. So I created one from scratch with the data closest
    to what the user provided.
    
    This is currently failing and we must fix

---
+++ src/test/spec/resource_not_found_bug_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'requests_helper'
+
+RSpec.configure do |c|
+  c.include RequestsHelper
+end
+
+RSpec.describe 'Resource not found bug' do
+  cap_1_name = 'Media 8h_O3'
+  cap_2_name = 'Media horaria_O3'
+  cap_3_name = 'Indice_O3'
+
+  let(:cap_description) { 'O3 measurement (µg/m³)' }
+  let(:cap_type) { 'sensor' }
+
+  before do
+    @responses = []
+    [cap_1_name, cap_2_name, cap_3_name].each do |name|
+      connection.delete("catalog/capabilities/#{name.gsub(' ', '%20')}")
+
+      @responses << connection.post(
+        'catalog/capabilities',
+        name: name,
+        description: cap_description,
+        capability_type: cap_type
+      )
+    end
+
+    resource_response = connection.post(
+      'adaptor/resources',
+      data: {
+        description: 'CETESB-Cid.Universitária-USP-Ipen-1',
+        capabilities: [cap_1_name, cap_2_name, cap_3_name],
+        status: 'active',
+        lat: -23.5521216,
+        lon: -46.932386
+      }
+    )
+    @responses << resource_response
+    @resource_uuid = response_json(resource_response)['data']['uuid']
+
+    (0..9).each do |i|
+      @responses << connection.post(
+        "adaptor/resources/#{@resource_uuid}/data",
+        data: {
+          environment_monitoring: [{ cap_1_name => 50, cap_2_name => '15', cap_3_name => '30', timestamp: "20/01/2019T10:27:3#{i}" }]
+        }
+      )
+    end
+  end
+
+  it 'is expected to have all successful responses' do
+    @responses.each do |response|
+      expect(response.status).to be >= 200
+      expect(response.status).to be <= 299
+    end
+  end
+
+  it "is expected to list the resource's historical data" do
+    response = connection.get("/collector/resources/#{@resource_uuid}/data")
+    expect(response.status).to eq(200)
+  end
+
+  after :all do
+    [cap_1_name, cap_2_name, cap_3_name].each do |name|
+      connection.delete("catalog/capabilities/#{name.gsub(' ', '%20')}")
+    end
+  end
+end
