fd1d0032d84652a424d3f975a6cf754b27326c11
Eduardo Silva <esilva.pragmacode@protonmail.ch>
2020-04-30 18:38:26 -0300

    Add tests for some simple default Rails files
    When configuring simplecov, it added to its covered files some default
    files that define some default classes. Here we add test files for them.

---
+++ src/actuator-controller/spec/mailers/application_mailer_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationMailer do
+  it 'is expected to be and ActionCable channel' do
+    expect(described_class).to be < ActionMailer::Base
+  end
+end
